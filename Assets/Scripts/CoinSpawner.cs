ee4b7896ede84a652de669323acf02f8be54dec1
Snuffko <kasOFF@mail.ru>
2020-02-27 12:38:09 +0300

    Delete magic numbers

--- Assets/Scripts/CoinSpawner.cs
+++ Assets/Scripts/CoinSpawner.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 [RequireComponent(typeof(BoxCollider2D))]
 public class CoinSpawner : Spawner
 {
+    [SerializeField] private int _maxCoinInLine;
+
     public override float GetHalfHeight()
     {
         return GetComponent<BoxCollider2D>().size.y / 2;
@@ -12,7 +14,7 @@ public class CoinSpawner : Spawner
 
     public override void Spawn(Vector3 position) 
     { 
-        for (int i = 0; i < Random.Range(1, 6); i++)
+        for (int i = 0; i < Random.Range(1, _maxCoinInLine); i++)
         {
             _currentPosition.x = position.x + i;
             CreateObject();
